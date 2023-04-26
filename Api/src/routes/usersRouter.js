1f8935b3dae25df993961f25a79860db2687e7b9
Armando1509 <armandogomez1509@gmail.com>
2023-04-25 18:53:56 -0600

    create routes of back

---
+++ Api/src/routes/usersRouter.js
@@ -0,0 +1,30 @@
+
+
+//userRouter/:id/vehiculo  
+
+//get general y post
+
+//userRouter/:id/vehiculo/:id  
+
+// get del vehico por id put 
+
+// user/datos 
+// vehiculo/datos
+
+// user/Jorge/vehiculos/datos
+ 
+const {Router} = require('express')
+
+
+const usersRouter = Router();
+
+usersRouter.get("/",/* HANDLER USER*/)
+usersRouter.post("/",/* HANDLER USER*/)
+
+usersRouter.get("vehicle",/* HANDLER VEHICLE */)
+usersRouter.post("vehicle",/* HANDLER VEHICLE */)
+
+usersRouter.get("vehicle/:id",/* HANDLER  VEHICLE BY ID*/)
+usersRouter.put("vehicle/:id",/* HANDLER VEHICLE BY ID*/)  
+
+module.exports= usersRouter
