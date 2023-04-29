f9bfca81d8da850e7556b3d296dcc44695df68cb
Camila Oyene <coyene63@gmail.com>
2023-04-29 00:45:29 -0300

    componente contactUs modificado

--- Client/src/components/ContactUs/ContactUs.jsx
+++ Client/src/components/ContactUs/ContactUs.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
-
+import React from "react";
+import './ContactUs.css'
 
 export default function ContactUs() {
-    const [showDescription, setShowDescription] = useState(false)
 
-    const toggleDescription = () => {
-        setShowDescription(!showDescription)
-    };
     return (
         <div className="Contactanos">
-            <button onClick={toggleDescription}>Contáctanos</button>
-            {
-                showDescription && (
-                    <div className="contac-us">
-                    <h2>Contáctanos</h2>
-                    <p>Puedes contactarnos en Whatsapp al 1121423423</p>
-                    </div>
-                )
-            }
+            <div className="contac-us">
+                <h2>Contáctanos</h2>
+                <p>Puedes contactarnos en Whatsapp al 1121423423</p>
+            </div>
         </div>
 
     )
