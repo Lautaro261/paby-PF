c20a7675b010dc3a5e055e699b3bd5830de36860
Camila Oyene <coyene63@gmail.com>
2023-05-03 15:41:52 -0300

    icono whatsapp

--- Client/src/components/ContactUs/ContactUs.jsx
+++ Client/src/components/ContactUs/ContactUs.jsx
@@ -7,7 +7,7 @@ export default function ContactUs() {
         <div className="Contactanos">
             <div className="contact-us">
                 <h2>Contáctanos</h2>
-                <p>Puedes contactarnos en Whatsapp al 1123232323</p>
+                <p>Puedes contactarnos en <i class="fab fa-whatsapp"></i>Whatsapp al 1123232323</p>
             </div>
         </div>
 
