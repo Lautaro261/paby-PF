3b2964827b954dc178d77b3dc0d84f03ad6069ce
Camila Oyene <coyene63@gmail.com>
2023-05-05 15:25:09 -0300

    Login y Logout con auth0

--- Client/src/components/LoginButton/LoginButton.jsx
+++ Client/src/components/LoginButton/LoginButton.jsx
@@ -1,9 +1,9 @@
 import {useAuth0} from "@auth0/auth0-react";
 
 const LoginButton= ()=>{
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithPopup } = useAuth0();
     return(
-        <button onClick={()=> loginWithRedirect()}>Ingresar</button>
+        <button onClick={()=>  loginWithPopup()}>Ingresar</button>
     )
 }
 
