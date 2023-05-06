a51eebb1260f613ffec58a6cf29416dff89b6a8e
Camila Oyene <coyene63@gmail.com>
2023-05-06 17:56:34 -0300

    actualizacion

--- Api/src/controllers/PutProfile.js
+++ Api/src/controllers/PutProfile.js
@@ -1,11 +1,7 @@
 const { Profile } = require("../db");
 
 const PutProfile = async (
-<<<<<<< HEAD
-  userId,
-=======
   sub,
->>>>>>> development
   nickname,
   phone,
   country,
@@ -13,17 +9,10 @@ const PutProfile = async (
   address,
   neighborhood
 ) => {
-<<<<<<< HEAD
-  const profileById = await Profile.findOne({ where: { userSub: userId } }); //AQUI TENGO EL PERFIL
-
-  if (!profileById) {
-    return "No se pudo editar el Perfil";
-=======
   const profileById = await Profile.findOne({ where: { userSub: sub } }); //AQUI TENGO EL PERFIL
 
   if (!profileById) {
     return null;
->>>>>>> development
   }
 
 
--- Api/src/handlers/handlerPutProfile.js
+++ Api/src/handlers/handlerPutProfile.js
@@ -2,19 +2,11 @@ const PutProfile = require("../controllers/PutProfile");
 
 const handlerPutProfile = async (req, res) => {
   try {
-<<<<<<< HEAD
-    const { userId, nickname, phone, country, city, address, neighborhood } =
-      req.body;
-
-    await PutProfile(
-      userId,
-=======
     const { sub, nickname, phone, country, city, address, neighborhood } =
       req.body;
 
     const upProfile = await PutProfile(
       sub,
->>>>>>> development
       nickname,
       phone,
       country,
@@ -23,11 +15,6 @@ const handlerPutProfile = async (req, res) => {
       neighborhood
     ); //AQUI TENGO EL PERFIL
 
-<<<<<<< HEAD
-    res.status(200).json({ message: "se modifico correctamente!" });
-  } catch (error) {
-    res.status(400).json({ message: "no se pudo modificar" });
-=======
     if (upProfile !== null) {
       res.status(200).json({ message: "se modifico correctamente!" });
     } else {
@@ -35,7 +22,6 @@ const handlerPutProfile = async (req, res) => {
     }
   } catch (error) {
     res.status(400).json({ message: "no se pudo modificar", error: error.message });
->>>>>>> development
   }
 };
 
