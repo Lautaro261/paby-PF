b4e8427de75dcb3bced181f11ac61bccc0679ec8
Juan Patricio <ggjuanpatricio@gmail.com>
2023-05-06 15:27:51 -0300

    Revert "ensamble"
    This reverts commit e12cdb9de0ba851eb65ed90780834531a9fdc052.

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -3,15 +3,13 @@ import parkingSpacesPaginationReducer from "./features/parkingSpacesPagination/p
 import parkingSpacesReducer from "./features/parkingSpaces/parkingSpacesSlice";
 import vehicleBrandReducer from "./features/vehicleBrand/vehicleBrandSlice";
 import parkingSpacesReservationReducer from './features/parkingSpacesReservation/parkingSpacesReservationSlice';
-import historyReducer from "./features/history/historySlice";
 
 const store = configureStore({
     reducer: {
         parkingSpacesPagination: parkingSpacesPaginationReducer,
         parkingSpaces: parkingSpacesReducer,
         parkingSpacesReservation: parkingSpacesReservationReducer,
-        vehicleBrand: vehicleBrandReducer,
-        history: historyReducer,
+        vehicleBrand: vehicleBrandReducer
     }
 });
 
