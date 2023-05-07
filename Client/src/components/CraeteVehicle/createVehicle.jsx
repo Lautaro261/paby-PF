e0da40794cd3093c816a3b8739c4028b533b9000
j10gof <juandiegoflorezlora@gmail.com>
2023-05-07 14:26:49 -0500

    Ver usuarios y editarlos listos

--- Client/src/components/CraeteVehicle/createVehicle.jsx
+++ Client/src/components/CraeteVehicle/createVehicle.jsx
@@ -6,10 +6,17 @@ import * as Yup from 'yup';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './createVehicle.module.css';
+import { useAuth0 } from '@auth0/auth0-react'
 import { Form as BSForm, FormGroup, FormLabel, FormControl, FormSelect, Button } from 'react-bootstrap';
 
 export default function CreateVehicle() {
-  const initialValues = {  
+  const { user, isLoading } = useAuth0();
+  if (isLoading) {
+    return (<div>Cargando...</div>);
+  };
+  console.log(user.sub)
+  const initialValues = { 
+    sub:'',
     vehicle_tipe:'',
     type_of_service: '',
     car_brand: '',
@@ -42,24 +49,25 @@ export default function CreateVehicle() {
     // Aquí iría la lógica de enviar los datos al servidor
     const handleSubmit = async (values, { resetForm }) => {
 
-      const licensePlate = values.license_plate;
+      //const licensePlate = values.license_plate;
       const requestData = {
+
+        sub: user.sub,
         license_plate_id: values.license_plate,
+        license_plate: values.license_plate,
         vehicle_tipe: values.vehicle_tipe,
         type_of_service: values.type_of_service,
         car_brand: values.car_brand,
         car_model: values.car_model,
         car_model_year: values.car_model_year,
         car_color: values.car_color,
-        license_plate: licensePlate,
-        license_plate_id: licensePlate,
         photo: values.photo,
       };
       
 
       try {
         console.log(requestData);
-        const response = await axios.post('/users/1/vehicle',requestData, {
+        const response = await axios.post('http://localhost:3001/users/create/vehicle',requestData, {
         
           headers: {
             'Content-Type': 'application/json'
