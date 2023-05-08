b9d6c383dad1a7e907d00dd599c103a4238f2cad
Camila Oyene <coyene63@gmail.com>
2023-05-07 23:29:36 -0300

    nueva vista Home

--- Client/src/components/dashboard/Dash.jsx
+++ Client/src/components/dashboard/Dash.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import styles from "./Dash.module.css";
+import React from 'react';
+import styles from './Dash.module.css';
 import { Link } from 'react-router-dom';
-import { useAuth0 } from "@auth0/auth0-react";
-import axios from 'axios'
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
 const Dash = () => {
   const { isAuthenticated, user } = useAuth0();
@@ -12,30 +12,46 @@ const Dash = () => {
     name: user?.name,
     email: user?.email,
     photo: user?.picture,
-}
+  };
 
-const sendDataToBackend = async () => {
+  const sendDataToBackend = async () => {
     try {
-        const response = await axios.post(`http://localhost:3001/users`, data , {
-                         headers: {
-                             'Content-Type': 'application/json'
-                         }})
-        return response 
+      const response = await axios.post('http://localhost:3001/users',data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+});
+      return response;
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
+
   return (
-    <div className={styles.panellateral}>
-      <ul>
-        <li><Link to="/parking-lot-filter" className={styles.textlink}>Hacer reserva</Link></li>
-        <li>{isAuthenticated ? <Link to='/profile'><button onClick={ ()=> sendDataToBackend()}>Perfil</button></Link> : false}</li>
-        <li>{isAuthenticated ? <Link to="/vehicles" className={styles.textlink}>Mis Vehiculos</Link>: false}</li>
-        <li>Métodos de pago</li>
-        <li>Soporte  en linea</li>
+    <div className={styles.dash}>
+      <ul className={styles.dashList}>
+        <li className={styles.dashItem}>
+          {isAuthenticated ? (<Link to="/profile" className={styles.dashLink}><button onClick={() => sendDataToBackend()}>Perfil</button></Link>) : null}
+        </li>
+        <li className={styles.dashItem}>
+          {isAuthenticated ? (<Link to="/vehicles" className={styles.dashLink}>Mis Vehiculos</Link>) : null}
+        </li>
+        <li className={styles.dashItem}>
+          {isAuthenticated ? (<span className={styles.dashLink}>Mis Reservas</span>) : null}
+        </li>
+        <li className={styles.dashItem}>
+          {isAuthenticated ? (<Link to="/parking-lot-filter" className={styles.dashLink}>Hacer reserva</Link>) : null}
+        </li>
+        <li className={styles.dashItem}>
+          {isAuthenticated ? (<span className={styles.dashLink}>Métodos de pago</span>) : null}
+        </li>
+        {/* <li className={styles.dashItem}>
+          {isAuthenticated ? (<span className={styles.dashLink}>Soporte en línea</span>) : null}
+        </li> */}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Dash
+export default Dash;
