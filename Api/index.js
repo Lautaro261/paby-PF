51db98f3ad61f3ed30923c0c3c64a5658ff59e61
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-05-09 22:29:27 -0300

    prueba de deploy martes 9

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-const { PORT } = process.env
+const { PORT } = process.env || 3001;
 // Hora y fecha de Colombia
 const moment = require("moment-timezone");
 
@@ -10,7 +10,7 @@ moment.tz.setDefault("America/Bogota");
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log("%s listening at", PORT); // eslint-disable-line no-console
   });
 });
 // true para eliminar BD y false para conservar
