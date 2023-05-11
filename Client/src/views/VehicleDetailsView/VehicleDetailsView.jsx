1975acafd71296318cf5e5cf35ffc9216a7a55bc
Camila Oyene <coyene63@gmail.com>
2023-05-11 03:01:21 -0300

    vista VehicleDetailsView

--- Client/src/views/VehicleDetailsView/VehicleDetailsView.jsx
+++ Client/src/views/VehicleDetailsView/VehicleDetailsView.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import Dash from "../../components/dashboard/Dash";
-import VehicleDetails from "../../components/VehicleDetails/vehicleDetails";
-
-const VehicleDetailsView = () => {
+import ViewVehicles from "../../components/Vehicles/Vehicles";
+const VehiclesView = () => {
     return (
         <div>
             <div><Navbar /> </div>
 
                 <div><Dash /></div>
             <div>
-                <div><VehicleDetails/></div>
+                <div><ViewVehicles /></div>
             </div>
 
             <div><Footer /></div>
@@ -19,4 +18,4 @@ const VehicleDetailsView = () => {
     )
 
 }
-export default VehicleDetailsView;
+export default VehiclesView;
