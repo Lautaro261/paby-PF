44aeeb02d69b1c4d131c2e8470c7cdb50a9c14c9
Camila Oyene <coyene63@gmail.com>
2023-05-13 23:19:13 -0300

    boton ingreso corregido

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         </li>
 
         <li className={style.buttonContainer}>
-          {isLoggedIn? null : <OwnLogin />}
+          {isLoggedIn || isAuthenticated ? null : <OwnLogin />}
         </li>
 
         <li className={style.buttonContainer}>
