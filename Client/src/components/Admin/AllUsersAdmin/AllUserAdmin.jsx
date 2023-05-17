e15302feeb8c4b1d4bd9b9afac043044962d2721
Camila Oyene <coyene63@gmail.com>
2023-05-17 13:57:23 -0300

    ok

--- Client/src/components/Admin/AllUsersAdmin/AllUserAdmin.jsx
+++ Client/src/components/Admin/AllUsersAdmin/AllUserAdmin.jsx
@@ -48,3 +48,63 @@ const AllUserAdmin = () => {
 }
 
 export default AllUserAdmin  
+
+
+
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getAllUserForAdmin } from "../../../redux/features/admin/adminSlice";
+// import User from "../User/User";
+
+// const AllUserAdmin = () => {
+//   const dispatch = useDispatch();
+//   const allUsers = useSelector((state) => state.admin.allUsers);
+//   const token = localStorage.getItem(`token`);
+//   const [sortDirection, setSortDirection] = useState("asc");
+
+//   useEffect(() => {
+//     dispatch(getAllUserForAdmin(token));
+//   }, [dispatch, token]);
+
+//   const toggleSortDirection = () => {
+//     setSortDirection((prevDirection) =>
+//       prevDirection === "asc" ? "desc" : "asc"
+//     );
+//   };
+
+//   const sortedUsers = [...allUsers].sort((a, b) =>
+//     sortDirection === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+//   );
+
+//   return (
+//     <div>
+//       <h2>Lista de clientes</h2>
+//       <button onClick={toggleSortDirection}>Cambiar orden</button>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Nombre</th>
+//             <th>Email</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {sortedUsers && sortedUsers.length ? (
+//             sortedUsers.map((user) => (
+//               <tr key={user.sub}>
+//                 <td>
+//                   <User email={user.email} name={user.name} sub={user.sub} />
+//                 </td>
+//               </tr>
+//             ))
+//           ) : (
+//             <tr>
+//               <td colSpan="2">Cargando</td>
+//             </tr>
+//           )}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default AllUserAdmin;
