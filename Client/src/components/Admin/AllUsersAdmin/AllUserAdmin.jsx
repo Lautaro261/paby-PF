9ce0f1edaf5ff21f9020950d788651c90ecd3e89
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-05-17 16:36:27 -0300

    lo de cami

--- Client/src/components/Admin/AllUsersAdmin/AllUserAdmin.jsx
+++ Client/src/components/Admin/AllUsersAdmin/AllUserAdmin.jsx
@@ -1,55 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllUserForAdmin } from "../../../redux/features/admin/adminSlice"
+import { getAllUserForAdmin } from "../../../redux/features/admin/adminSlice";
 import User from "../User/User";
 
 const AllUserAdmin = () => {
-    const dispatch = useDispatch()
-    const allUsers = useSelector(state => state.admin.allUsers)
-    const token = localStorage.getItem(`token`)
-    const [sortDirection, setSortDirection] = useState('asc')
+  const dispatch = useDispatch();
+  const allUsers = useSelector((state) => state.admin.allUsers);
+  const token = localStorage.getItem(`token`);
+//   const [sortDirection, setSortDirection] = useState("asc");
 
-    useEffect(()=>{
-        console.log(token, 'token de allUser')
-        dispatch(getAllUserForAdmin(token))
-    },[dispatch,token])
+  useEffect(() => {
+    console.log(token, "token de allUser");
+    dispatch(getAllUserForAdmin(token));
+  }, [dispatch, token]);
 
-    const toggleSortDirection = () => {
-        setSortDirection((prevDirection)=>
-            prevDirection === 'asc' ? 'desc' : 'asc'
-        )
-    }
+//   const toggleSortDirection = () => {
+//     setSortDirection((prevDirection) =>
+//       prevDirection === "asc" ? "desc" : "asc"
+//     );
+//   };
 
-    const sortedUsers = [...allUsers].sort((a, b) => 
-    sortDirection === 'asc' ? a.name.localeCompare(b.name): b.name.localeCompare(a.name)
-    )
+//   const users = [...allUsers];
+//   const sortedUsers = users.sort((a, b) =>
+//     sortDirection === "asc"
+//       ? a.name.localeCompare(b.name)
+//       : b.name.localeCompare(a.name)
+//   );
 
-    return(
-        <div>
-            <h2>Lista de clientes</h2>
-            <button onClick={toggleSortDirection}>Cambiar orden</button>
-            <div>
-            {
-               sortedUsers && sortedUsers.length ? 
-              sortedUsers.map((user)=>{
-                    return(
-                        <div key={user.sub}>
-                            <User email={user.email} name={user.name} sub={user.sub}/>
-                        </div>
-                    )
-                }):
-                <div>
-                    <p>Cargando</p>
-                </div>
-            }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Lista de clientes</h2>
+      <button >Cambiar orden</button>
+      <div>
+        { allUsers && allUsers.length ? (
+          allUsers.map((user) => {
+            return (
+              <div key={user.sub}>
+                <User email={user.email} name={user.name} sub={user.sub} />
+              </div>
+            );
+          })
+        ) : (
+          <div>
+            <p>Cargando</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default AllUserAdmin  
-
-
+export default AllUserAdmin;
 
 // import React, { useEffect, useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
